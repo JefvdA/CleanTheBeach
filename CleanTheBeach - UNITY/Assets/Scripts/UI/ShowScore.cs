0b4103ad000fad992cd2a9986be909217074c4a4
Stef Santens <stef.santens@gmail.com>
2022-01-23 13:10:44 +0100

    Cleaned up scripts

--- CleanTheBeach - UNITY/Assets/Scripts/UI/ShowScore.cs
+++ CleanTheBeach - UNITY/Assets/Scripts/UI/ShowScore.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class test : MonoBehaviour
+public class ShowScore : MonoBehaviour
 {
     public Text Score;
     // Start is called before the first frame update
